d0a76e911dae50186c539a5d2aff20f2d5a4ac03
hiwonwon <hyewon9913@gmail.com>
2024-05-15 22:40:25 +0900

    first commit

---
+++ src/main/java/com/appproject/whatdoyoumeme/WhatdoyoumemeApplication.java
@@ -0,0 +1,13 @@
+package com.appproject.whatdoyoumeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatdoyoumemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatdoyoumemeApplication.class, args);
+	}
+
+}
